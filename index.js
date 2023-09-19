bf713d96563a0321cfbb0c612cffeeeb616b6d5e
Muhammad-Yusuf-2766 <strangerbellimo@gmail.com>
2023-09-19 18:48:54 +0900

    master

--- index.js
+++ index.js
@@ -1,52 +1,62 @@
-// Node js modullari. Node js da modullar 3 turga bo'linadi. 1: Core Modul. 2: External moudl. 3:File Modul
-// Coder Modulni ko'tib chiqamiz. Avval terminslda npm init -- yes burugini beramiz. bu bizga j.son package ni ochib beradi. bu pacge bizga dasturimiz haqida hamma malumotlarni beradi. Qaysi library, qaysi dastur va hokazolardan foydalanganimiz hammasi chiqadi. albata u malumotlarni avval o'zimiz kiritishimiz kerak.
-// 1: Core Modul. bu asosiy modul hisoblanadi. SetTimeOut modulni ko'ramiz. bu modul o'ziga 2 ta parametrni oladi. biri function 2- si vaqt. kiriltilgan vaqt o'tgach 1 - parametrni ishga tushiradi. (functionni) M:
-setTimeout(function() {
-    console.log("Ishga tushirildi.")
-  }, 3000);
+// External Modullar. Bu modullarni core moduldan farqi, ularni ishlatish uchun j.son package ga instal qilib olishimiz kerak. Uning uchun terminalda "npm install modulname --save" commandasini beramiz va uni chaqirib olamiz.
+const moment = require("moment");
+const time = moment().format();
+console.log("Hozirgi vaqt: ", time);
+
+
+
+// inquirer modulini ko'ramiz.
+const inquirer = require("inquirer");
+  inquirer
+  .prompt([{type: 'input', name: 'son', message: 'raqamni kiriting..'}])
+  .then(answer =>{
+    console.log("Siz kiritgan raqam: ", answer.son);
+  })
+  .catch(err => console.log(err));
+
+
+
+
+  // Validator modulni ko'ramiz: bu turli malumot turlarini tekshiradi. Email. number.string. IP address
+  const validator = require("validator");
+  const email = validator.isEmail("strangerbellimo@gmail.com");
+  console.log("Email qiymati: ", email);
+
+  const ip = validator.isIP('14.42.86.31');
+  console.log("Sizning ip manzilingiz:", ip);
+
+
+
+  // uuid: moduli. bu bizga random stringlarni qaytarishi mumkin. 
+  const { v4: uuidv4 } = require('uuid');
+  const random = uuidv4();
+  console.log("random string: ", random);
+
+
+
+
+  // chalk: moduli. bu modul orqali natijani turli ko'rinishda ko'rishimiz mumkin.
+  const chalk = require('chalk');
+  const log = console.log;
+  log(chalk.bgGreen.white("Your password:") + chalk.blue("",random) + chalk.redBright(" !"));
   
-  // SetInterval moduli. Bu Modul ham berilgan vaqtdan so'ng functionni ishga tushiradi. lekin functionni qayta qayta ishga tushiravedi. Yani Loop M:
-  let number = 0;
-  const IntervalId = setInterval(function () {
-    console.log("sekund", number);
-    number++
-    
-    if(number > 5){
-      clearInterval(IntervalId); // Stop the interval when number reaches 6
-    };
-  },1000);
-  
-  
-  // fs moduli. (File system), Bu modul dasturni ichidagi filelarni qo'lga olib berishda ishlatiladi. bu modulni ishlatish uchun require syntax si orqali chaqirib olsihimiz kerak.M:
-  const fs = require ('fs');
-  const { networkInterfaces } = require ('os')
-  const data = fs.readFileSync('./input.txt', 'utf-8');
-  console.log(data);
-  
-  // endi faylga o'zgartirish kiritamiz.
-  // fs.writeFileSync('./input.txt', `${data} \n\t\t Qashqirlar Makoni:`);
-  const new_data = fs.writeFileSync('./input.txt', `${data} \n\t\t Qashqirlar Makoni:`);
-  console.log(new_data);
-  
-  
-  // ##############  Task - B  ####################//
-  // Functionda 1 parametr bo'lsin. u parametrda string ko'rinishida harflar va sonlar bo'lsin. consolega esa stringda nechta son qatnashgan bo'lsa shuni chiqarsin. 
-  
-  function getNumbers (malumot) {
-    let number = 0;
-    let raqamlar = "0123456789";
-  
-    for(i = 0; i < malumot.length; i++){
-      const raqam = malumot.charAt(i);
-      if(raqamlar.includes(raqam)){
-        number++
-      }
-    }
-    return number;
-  }
-  
-  const result = getNumbers("ash3n1mk5llnhn65l4j309");
-  console.log(result);
-  
-  
-  
+  console.log("=================== C-TASK =====================");
+
+
+
+
+
+
+// C-Task. 2 ta stringni tekshirish. 
+function checkString(str1, str2) {
+  str1 = str1.replace('').toLowerCase();
+  str2 = str2.replace('').toLowerCase();
+
+  const sortedStr1 = str1.split('').sort().join('');
+  const sortedStr2 = str2.split('').sort().join('');
+
+  return sortedStr1 === sortedStr2;
+}
+
+const result = checkString("internation", "ntieraontin");
+console.log("stringlar qiymati:", result); 
