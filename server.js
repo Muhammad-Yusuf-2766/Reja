619177149e4a42e5601eb4f130ccca8ee97a6e2a
Muhammad-Yusuf-2766 <strangerbellimo@gmail.com>
2023-09-23 17:10:40 +0900

    17 -Dars. ejs framework.vazifa

--- server.js
+++ server.js
@@ -1,79 +1,5 @@
-// External Modullar. Bu modullarni core moduldan farqi, ularni ishlatish uchun j.son package ga instal qilib olishimiz kerak. Uning uchun terminalda "npm install modulname --save" commandasini beramiz va uni chaqirib olamiz.
-const moment = require("moment");
-const time = moment().format();
-console.log("Hozirgi vaqt: ", time);
+// ============================================= 17-DARS. EXPRESS.JS framework. and Web server =================================//
 
-
-
-// inquirer modulini ko'ramiz.
-const inquirer = require("inquirer");
-  inquirer
-  .prompt([{type: 'input', name: 'son', message: 'raqamni kiriting..'}])
-  .then(answer =>{
-    console.log("Siz kiritgan raqam: ", answer.son);
-  })
-  .catch(err => console.log(err));
-
-
-
-
-  // Validator modulni ko'ramiz: bu turli malumot turlarini tekshiradi. Email. number.string. IP address
-  const validator = require("validator");
-  const email = validator.isEmail("strangerbellimogmail.com");
-  console.log("Email qiymati: ", email);
-
-  const ip = validator.isIP('14.42.86.31');
-  console.log("Sizning ip manzilingiz:", ip);
-
-
-
-  // uuid: moduli. bu bizga random stringlarni qaytarishi mumkin. 
-  const { v4: uuidv4 } = require('uuid');
-  const random = uuidv4();
-  console.log("random string: ", random);
-
-
-
-
-  // chalk: moduli. bu modul orqali natijani turli ko'rinishda ko'rishimiz mumkin.
-  const chalk = require('chalk');
-  const log = console.log;
-  log(chalk.bgGreen.white("Your password:") + chalk.blue("",random) + chalk.redBright(" !"));
-  
-  console.log("================= File Modullar ================");
-
-
-  //================== File Modullar =====================//
-  // file modullar bu biz yaratgan fayl modullar hisoblanadi. va fayllar bilan ishlanadi.M:
-  const   calculate = require ('./hisob.js');
-
-  const natija = calculate.plus(200, 300);
-  console.log("Sonlar yig'indisi: ", natija);
-  console.log("=========================");
-
-  const natija2 = calculate.minus(500, 270);
-  console.log("Sonlar ayirmasi:", natija2);
-  console.log("==========================");
-
-  const natija3 = calculate.bolish(500, 10);
-  console.log("Sonlan bo'linmasi: ", natija3);
-  console.log("===========================");
-
-  const natija4 = calculate.kopaytirish(500, 2);
-  console.log("Sonlar ko'paytmasi: ", natija4);
-
-// endi account.js file dagi dasturni chaqirib ishga tushiramiz.
-  const Account = require('./account.js');
-  Account.tellMeTime();
-  Account.tellMeAboutClass();
-
-  const myAccount = new Account("Yusuf", 10000, 90343243872);
-  myAccount.showDetails();
-  myAccount.deposit(200);
-  myAccount.sendingMoney(450);
-
-
-// ============================================= EXPRESS.JS framework. and Web server =================================//
 console.log("======================= EXPRESS.JS AND WEB SERVER ========================");
 
 console.log("Web serverni boshlaymiz.")
@@ -107,3 +33,82 @@ let PORT = 8080;
 server.listen(PORT, function () {
   console.log(`The Sever is running  successfully on port ${PORT}`);
 })
+
+
+
+
+
+
+// //External Modullar. Bu modullarni core moduldan farqi, ularni ishlatish uchun j.son package ga instal qilib olishimiz kerak. Uning uchun terminalda "npm install modulname --save" commandasini beramiz va uni chaqirib olamiz.
+// const moment = require("moment");
+// const time = moment().format();
+// console.log("Hozirgi vaqt: ", time);
+
+
+
+// //inquirer modulini ko'ramiz.
+// const inquirer = require("inquirer");
+//   inquirer
+//   .prompt([{type: 'input', name: 'son', message: 'raqamni kiriting..'}])
+//   .then(answer =>{
+//     console.log("Siz kiritgan raqam: ", answer.son);
+//   })
+//   .catch(err => console.log(err));
+
+
+
+
+ // // Validator modulni ko'ramiz: bu turli malumot turlarini tekshiradi. Email. number.string. IP address
+  // const validator = require("validator");
+  // const email = validator.isEmail("strangerbellimogmail.com");
+  // console.log("Email qiymati: ", email);
+
+  // const ip = validator.isIP('14.42.86.31');
+  // console.log("Sizning ip manzilingiz:", ip);
+
+
+
+  //// uuid: moduli. bu bizga random stringlarni qaytarishi mumkin. 
+  // const { v4: uuidv4 } = require('uuid');
+  // const random = uuidv4();
+  // console.log("random string: ", random);
+
+
+
+
+ // // chalk: moduli. bu modul orqali natijani turli ko'rinishda ko'rishimiz mumkin.
+  // const chalk = require('chalk');
+  // const log = console.log;
+  // log(chalk.bgGreen.white("Your password:") + chalk.blue("",random) + chalk.redBright(" !"));
+  
+  // console.log("================= File Modullar ================");
+
+
+ // //================== File Modullar =====================//
+ // // file modullar bu biz yaratgan fayl modullar hisoblanadi. va fayllar bilan ishlanadi.M:
+  // const   calculate = require ('./hisob.js');
+
+  // const natija = calculate.plus(200, 300);
+  // console.log("Sonlar yig'indisi: ", natija);
+  // console.log("=========================");
+
+  // const natija2 = calculate.minus(500, 270);
+  // console.log("Sonlar ayirmasi:", natija2);
+  // console.log("==========================");
+
+  // const natija3 = calculate.bolish(500, 10);
+  // console.log("Sonlan bo'linmasi: ", natija3);
+  // console.log("===========================");
+
+  // const natija4 = calculate.kopaytirish(500, 2);
+  // console.log("Sonlar ko'paytmasi: ", natija4);
+
+//// endi account.js file dagi dasturni chaqirib ishga tushiramiz.
+  // const Account = require('./account.js');
+  // Account.tellMeTime();
+  // Account.tellMeAboutClass();
+
+  // const myAccount = new Account("Yusuf", 10000, 90343243872);
+  // myAccount.showDetails();
+  // myAccount.deposit(200);
+  // myAccount.sendingMoney(450);
